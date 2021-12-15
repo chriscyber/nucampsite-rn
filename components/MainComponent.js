c8f444ef98f71b50eeb01240bb6b1a75098d8ee9
Christine Anthony <2christineanthony@gmail.com>
2021-12-15 18:52:00 -0500

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Constants from 'expo-constants';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator( //set which components are available to the stack. 
@@ -25,7 +27,42 @@ const DirectoryNavigator = createStackNavigator( //set which components are avai
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator); //returns react component that handles connecting top level navigator to  native environment, like native back button. Usually wraps top level navigator.
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    }, 
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+)
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CDC8FF'
+    }
+)
+
+export const MainComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+const AppNavigator = createAppContainer(MainNavigator); //returns react component that handles connecting top level navigator to  native environment, like native back button. Usually wraps top level navigator.
 
 class Main extends Component {
     render() {
