f494de7701aaf0ef1bc6f9e9a93c435880ad32ff
Christine Anthony <2christineanthony@gmail.com>
2021-12-24 11:28:02 -0500

    Setting up Redux

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
+import React, { Component } from "react";
+import { FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
 class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
+  static navigationOptions = {
+    //static is keyword that sets method on class as opposed to object from class.
+    title: "Directory", // tells navigator the title of nav option
+  };
 
-    static navigationOptions = { //static is keyword that sets method on class as opposed to object from class.
-        title: 'Directory' // tells navigator the title of nav option
-    }
+  render() {
+    const { navigate } = this.props.navigation; //destructuring
+    const renderDirectoryItem = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })} //ListItem comes with built in onPress prop. When this component pressed, this function will fire.
+          leftAvatar={{ source: require("./images/react-lake.jpg") }}
+        />
+      );
+    };
 
-    render() {
-        const { navigate } = this.props.navigation; //destructuring
-        const renderDirectoryItem = ({item}) => {
-            return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })} //ListItem comes with built in onPress prop. When this component pressed, this function will fire. 
-                    leftAvatar={{ source: require('./images/react-lake.jpg')}}
-                />
-            );
-        };
-
-        return (
-            <FlatList
-                data={this.state.campsites}
-                renderItem={renderDirectoryItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        );
-    }
+    return (
+      <FlatList
+        data={this.state.campsites}
+        renderItem={renderDirectoryItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
+  }
 }
 
 export default Directory;
---
+++ redux/ActionCreators.js
@@ -0,0 +1,161 @@
+// An action is an event that describes something that happened in the app. So these are the events that can change state. The payload is additional info about what happened (in addition to type). Action passed to reducers. dispatch is a redux store method; the only way to update state is to call store.dispatch and pass in action object (line 23)
+
+import * as ActionTypes from "./ActionTypes";
+import { baseUrl } from "../shared/baseUrl";
+
+export const fetchComments = () => (dispatch) => {
+  // the function inside the dispatch function stops action from going to reducer while middleware uses fetch (async)
+  return fetch(baseUrl + "comments")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((comments) => dispatch(addComments(comments)))
+    .catch((error) => dispatch(commentsFailed(error.message)));
+};
+
+export const commentsFailed = (errMess) => ({
+  //this is the action creator
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errMess,
+});
+
+export const addComments = (comments) => ({
+  //this is the action creator
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments,
+});
+
+export const fetchCampsites = () => (dispatch) => {
+  dispatch(campsitesLoading());
+
+  return fetch(baseUrl + "campsites")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((campsites) => dispatch(addCampsites(campsites)))
+    .catch((error) => dispatch(campsitesFailed(error.message)));
+};
+
+export const campsitesLoading = () => ({
+  type: ActionTypes.CAMPSITES_LOADING,
+});
+
+export const campsitesFailed = (errMess) => ({
+  type: ActionTypes.CAMPSITES_FAILED,
+  payload: errMess,
+});
+
+export const addCampsites = (campsites) => ({
+  type: ActionTypes.ADD_CAMPSITES,
+  payload: campsites,
+});
+
+export const fetchPromotions = () => (dispatch) => {
+  dispatch(promotionsLoading());
+
+  return fetch(baseUrl + "promotions")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((promotions) => dispatch(addPromotions(promotions)))
+    .catch((error) => dispatch(promotionsFailed(error.message)));
+};
+
+export const promotionsLoading = () => ({
+  type: ActionTypes.PROMOTIONS_LOADING,
+});
+
+export const promotionsFailed = (errMess) => ({
+  type: ActionTypes.PROMOTIONS_FAILED,
+  payload: errMess,
+});
+
+export const addPromotions = (promotions) => ({
+  type: ActionTypes.ADD_PROMOTIONS,
+  payload: promotions,
+});
+
+export const fetchPartners = () => (dispatch) => {
+  dispatch(partnersLoading());
+
+  return fetch(baseUrl + "partners")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((partners) => dispatch(addPartners(partners)))
+    .catch((error) => dispatch(partnersFailed(error.message)));
+};
+
+export const partnersLoading = () => ({
+  type: ActionTypes.PARTNERS_LOADING,
+});
+
+export const partnersFailed = (errMess) => ({
+  type: ActionTypes.PARTNERS_FAILED,
+  payload: errMess,
+});
+
+export const addPartners = (partners) => ({
+  type: ActionTypes.ADD_PARTNERS,
+  payload: partners,
+});
